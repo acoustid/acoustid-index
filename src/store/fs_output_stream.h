93f0cdc2d7837d5312d7eb850175876937a24785
Lukáš Lalinský <lalinsky@gmail.com>
2011-03-20 19:11:34 +0100

    Relicense to the MIT license

--- src/store/fs_output_stream.h
+++ src/store/fs_output_stream.h
@@ -1,18 +1,5 @@
-// Acoustid Index -- Inverted index for audio fingerprints
 // Copyright (C) 2011  Lukas Lalinsky
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Distributed under the MIT license, see the LICENSE file for details.
 
 #ifndef ACOUSTID_FS_OUTPUT_STREAM_H_
 #define ACOUSTID_FS_OUTPUT_STREAM_H_
