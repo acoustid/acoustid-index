a0c237f535890449c14c31e56b8bd377c0490b2f
Lukáš Lalinský <lalinsky@gmail.com>
2011-05-02 12:02:19 +0200

    Two-phase segment search to avoid reading blocks multiple times

--- src/index/segment_searcher.cpp
+++ src/index/segment_searcher.cpp
@@ -21,32 +21,42 @@ SegmentSearcher::~SegmentSearcher()
 
 void SegmentSearcher::search(uint32_t *fingerprint, size_t length, Collector *collector)
 {
-	// XXX do this in two phases, optimizing multiple reads of the same block
+	if (!length) {
+		return;
+	}
+
 	size_t lastReadBlock = ~0;
 	size_t readBlockCount = 0;
-	//qSort(fingerprint, fingerprint + length);
+	qSort(fingerprint, fingerprint + length);
+	QList<size_t> blocks;
 	for (size_t i = 0; i < length; i++) {
 		size_t firstBlock, lastBlock;
-		//qDebug() << "searching for" << fingerprint[i];
 		if (m_index->search(fingerprint[i], &firstBlock, &lastBlock)) {
-			//qDebug() << "found blocks" << firstBlock << lastBlock;
-			/*if (readBlockCount > 0 && lastReadBlock >= firstBlock) {
+			if (readBlockCount > 0 && lastReadBlock >= firstBlock) {
 				firstBlock = lastReadBlock + 1;
-			}*/
+			}
 			for (size_t block = firstBlock; block <= lastBlock; block++) {
-				uint32_t firstKey = m_index->levelKey(block);
-				ScopedPtr<BlockDataIterator> blockData(m_dataReader->readBlock(block, firstKey));
-				while (blockData->next()) {
-					uint32_t key = blockData->key();
-					if (key >= fingerprint[i]) {
-						if (key == fingerprint[i]) {
-							collector->collect(blockData->value());
-						}
-						break;
-					}
-				}
+				blocks.append(block);
 				readBlockCount++;
 				lastReadBlock = block;
+			}
+		}
+	}
+	size_t i = 0;
+	for (size_t b = 0; b < blocks.size(); b++) {
+		size_t block = blocks.at(b);
+		uint32_t firstKey = m_index->levelKey(block);
+		ScopedPtr<BlockDataIterator> blockData(m_dataReader->readBlock(block, firstKey));
+		while (blockData->next()) {
+			uint32_t key = blockData->key();
+			while (key > fingerprint[i]) {
+				i++;
+				if (i >= length) {
+					return;
+				}
+			}
+			if (key == fingerprint[i]) {
+				collector->collect(blockData->value());
 			}
 		}
 	}
