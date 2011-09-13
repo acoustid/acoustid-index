34489baca16a649c411484acbd1bb994981e501a
Lukáš Lalinský <lalinsky@gmail.com>
2011-09-13 11:15:19 +0200

    Three segments-by-tier by default

--- src/common.h
+++ src/common.h
@@ -27,7 +27,7 @@ namespace Acoustid {
 static const int MAX_SEGMENT_BUFFER_SIZE = 1024 * 1024 * 5;
 static const int BLOCK_SIZE = 512;
 static const int MAX_MERGE_AT_ONCE = 4;
-static const int MAX_SEGMENTS_PER_TIER = 4;
+static const int MAX_SEGMENTS_PER_TIER = 3;
 static const int MAX_SEGMENT_BLOCKS = 2 * 1024 * 1024;
 static const int FLOOR_SEGMENT_BLOCKS = 1024;
 
