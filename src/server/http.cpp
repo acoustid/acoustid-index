c047cb2b0abf471c356179960604915546a058d3
Lukáš Lalinský <lalinsky@gmail.com>
2021-12-02 15:39:09 +0100

    Working on HTTP Index API

--- src/server/http.cpp
+++ src/server/http.cpp
@@ -1,46 +1,92 @@
 #include "http.h"
+
 #include "metrics.h"
 
+using namespace qhttp;
 using namespace qhttp::server;
 
 namespace Acoustid {
 namespace Server {
 
-static void sendContent(QHttpResponse *res, const QString &content) {
+static void sendStringResponse(qhttp::TStatusCode status, QHttpResponse *res, const QString &content) {
     auto contentBytes = content.toUtf8();
+    res->setStatusCode(status);
     res->addHeaderValue("Content-Length", contentBytes.size());
     res->end(contentBytes);
 }
 
-void handleHttpRequest(QHttpRequest *req, QHttpResponse *res, QSharedPointer<Metrics> metrics) {
-    auto url = req->url();
-    if (url.path() == "/metrics") {
-        auto content = metrics->toStringList().join("\n") + "\n";
-        res->setStatusCode(qhttp::ESTATUS_OK);
-        sendContent(res, content);
-    } else if (url.path() == "/health/ready") {
-        res->setStatusCode(qhttp::ESTATUS_OK);
-        res->addHeader("Content-Type", "text/plain; charset=utf-8");
-        sendContent(res, "OK\n");
-    } else if (url.path() == "/health/alive") {
-        res->setStatusCode(qhttp::ESTATUS_OK);
-        res->addHeader("Content-Type", "text/plain; charset=utf-8");
-        sendContent(res, "OK\n");
-    } else {
-        res->setStatusCode(qhttp::ESTATUS_NOT_FOUND);
-        sendContent(res, "");
-    }
-}
-
 HttpRequestHandler::HttpRequestHandler(QSharedPointer<Index> index, QSharedPointer<Metrics> metrics)
-    : m_index(index), m_metrics(metrics)
-{
+    : m_index(index), m_metrics(metrics) {
+    // Healthchecks
+    addHandler(qhttp::EHTTP_GET, "/_health/alive",
+               [this](QHttpRequest *req, QHttpResponse *res) { sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n"); });
+    addHandler(qhttp::EHTTP_GET, "/_health/ready",
+               [this](QHttpRequest *req, QHttpResponse *res) { sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n"); });
+
+    // Prometheus metrics
+    addHandler(qhttp::EHTTP_GET, "/_metrics", [this](QHttpRequest *req, QHttpResponse *res) {
+        auto content = m_metrics->toStringList().join("\n") + "\n";
+        sendStringResponse(qhttp::ESTATUS_OK, res, content);
+    });
+
+    // Index API
+    const auto indexPatternPrefix = "/(?<index>[^_/][^/]*)";
+    addHandler(qhttp::EHTTP_HEAD, indexPatternPrefix,
+               [this](QHttpRequest *req, QHttpResponse *res, const QMap<QString, QString> &args) {
+                   const auto indexName = args["index"];
+                   qDebug() << "Checking status of index" << indexName;
+                   sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n");
+               });
+    addHandler(qhttp::EHTTP_GET, indexPatternPrefix,
+               [this](QHttpRequest *req, QHttpResponse *res, const QMap<QString, QString> &args) {
+                   const auto indexName = args["index"];
+                   qDebug() << "Getting index" << indexName;
+                   sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n");
+               });
+    addHandler(qhttp::EHTTP_PUT, indexPatternPrefix,
+               [this](QHttpRequest *req, QHttpResponse *res, const QMap<QString, QString> &args) {
+                   const auto indexName = args["index"];
+                   qDebug() << "Creating index" << indexName;
+                   sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n");
+               });
+    addHandler(qhttp::EHTTP_DELETE, indexPatternPrefix,
+               [this](QHttpRequest *req, QHttpResponse *res, const QMap<QString, QString> &args) {
+                   const auto indexName = args["index"];
+                   qDebug() << "Deleting index" << indexName;
+                   sendStringResponse(qhttp::ESTATUS_OK, res, "ok\n");
+               });
 }
 
-void HttpRequestHandler::handleRequest(qhttp::server::QHttpRequest *req, qhttp::server::QHttpResponse *res)
-{
-    handleHttpRequest(req, res, m_metrics);
+void HttpRequestHandler::addHandler(qhttp::THttpMethod method, const QString &pattern, Handler handler) {
+    m_handlers.push_back(
+        {method, QRegularExpression(pattern),
+         [=](QHttpRequest *req, QHttpResponse *res, QMap<QString, QString> args) { handler(req, res); }});
 }
 
-} // namespace Server
-} // namespace Acoustid
+void HttpRequestHandler::addHandler(qhttp::THttpMethod method, const QString &pattern, HandlerWithArgs handler) {
+    m_handlers.push_back({method, QRegularExpression(pattern), handler});
+}
+
+void HttpRequestHandler::handleRequest(QHttpRequest *req, QHttpResponse *res) {
+    for (auto it = m_handlers.begin(); it != m_handlers.end(); ++it) {
+        const auto method = std::get<0>(*it);
+        const auto pattern = std::get<1>(*it);
+        const auto handler = std::get<2>(*it);
+        if (method == req->method()) {
+            const auto match = pattern.match(req->url().path());
+            if (match.hasMatch()) {
+                QMap<QString, QString> args;
+                auto argNames = pattern.namedCaptureGroups();
+                for (auto i = 1; i < argNames.size(); ++i) {
+                    args[argNames[i]] = match.captured(i);
+                }
+                handler(req, res, args);
+                return;
+            }
+        }
+    }
+    sendStringResponse(qhttp::ESTATUS_NOT_FOUND, res, "not found\n");
+}
+
+}  // namespace Server
+}  // namespace Acoustid
--- src/server/http.h
+++ src/server/http.h
@@ -4,7 +4,9 @@
 #ifndef ACOUSTID_SERVER_HTTP_H_
 #define ACOUSTID_SERVER_HTTP_H_
 
+#include <QRegularExpression>
 #include <QSharedPointer>
+
 #include "qhttpserver.hpp"
 #include "qhttpserverrequest.hpp"
 #include "qhttpserverresponse.hpp"
@@ -25,16 +27,22 @@ class HttpRequestHandler : public QObject {
 
     void handleRequest(qhttp::server::QHttpRequest *req, qhttp::server::QHttpResponse *res);
 
-    void operator()(qhttp::server::QHttpRequest *req, qhttp::server::QHttpResponse *res) {
-        handleRequest(req, res);
-    }
-
  private:
     QSharedPointer<Index> m_index;
     QSharedPointer<Metrics> m_metrics;
+
+    typedef std::function<void(qhttp::server::QHttpRequest *, qhttp::server::QHttpResponse *)> Handler;
+    typedef std::function<void(qhttp::server::QHttpRequest *, qhttp::server::QHttpResponse *,
+                               QMap<QString, QString> args)>
+        HandlerWithArgs;
+
+    void addHandler(qhttp::THttpMethod, const QString &pattern, Handler handler);
+    void addHandler(qhttp::THttpMethod, const QString &pattern, HandlerWithArgs handler);
+
+    std::vector<std::tuple<qhttp::THttpMethod, QRegularExpression, HandlerWithArgs>> m_handlers;
 };
 
-} // namespace Server
-} // namespace Acoustid
+}  // namespace Server
+}  // namespace Acoustid
 
 #endif
