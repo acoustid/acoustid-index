5b1f4e6a450997a930098b0858a4001cf63e401a
Lukáš Lalinský <lalinsky@gmail.com>
2021-12-08 12:14:21 +0100

    Remove useless debug output

--- src/index/segment_docs.cpp
+++ src/index/segment_docs.cpp
@@ -12,13 +12,10 @@ SegmentDocs::~SegmentDocs() {}
 std::optional<std::pair<uint32_t, bool>> SegmentDocs::findDocumentUpdate(uint32_t docId,
                                                                          uint32_t currentVersion) const {
     auto it = m_docs.find(docId);
-    qDebug() << "it" << *it;
     if (it == m_docs.end()) {
-        qDebug() << docId << "not found";
         return std::nullopt;
     }
     if (currentVersion >= it->second.first) {
-        qDebug() << docId << "too old";
         return std::nullopt;
     }
     return std::make_pair(it->second.first, it->second.second);
