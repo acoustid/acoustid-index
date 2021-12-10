c825ab126b333160c5f903e87b284de787cf64bb
Lukáš Lalinský <lalinsky@gmail.com>
2021-12-10 05:58:24 +0100

    Backport some of the smaller changes from v2

--- src/index/index.h
+++ src/index/index.h
@@ -4,14 +4,15 @@
 #ifndef ACOUSTID_INDEX_H_
 #define ACOUSTID_INDEX_H_
 
+#include <QDeadlineTimer>
 #include <QMutex>
 #include <QWaitCondition>
-#include <QDeadlineTimer>
+
 #include "common.h"
 #include "index.h"
 #include "index_info.h"
-#include "store/directory.h"
 #include "segment_index.h"
+#include "store/directory.h"
 
 namespace Acoustid {
 
@@ -23,53 +24,46 @@ class IndexWriter;
 //
 // This class is thread-safe and is intended to be shared by multiple
 // threads. Threads can use it to open their own searchers or writers.
-class Index : public QEnableSharedFromThis<Index>
-{
-public:
-	// Build a new instance using the given directory
-	Index(DirectorySharedPtr dir, bool create = false);
-	virtual ~Index();
+class Index : public QEnableSharedFromThis<Index> {
+ public:
+    // Build a new instance using the given directory
+    Index(DirectorySharedPtr dir, bool create = false);
+    virtual ~Index();
 
-	// Return the directory which contains the index data
-	DirectorySharedPtr directory()
-	{
-		return m_dir;
-	}
+    // Return the directory which contains the index data
+    DirectorySharedPtr directory() { return m_dir; }
 
-	IndexInfo info()
-	{
-		return m_info;
-	}
+    IndexInfo info() { return m_info; }
 
     QSharedPointer<IndexReader> openReader();
     QSharedPointer<IndexWriter> openWriter(bool wait = false, int64_t timeoutInMSecs = 0);
 
-	void acquireWriterLock(bool wait = false, int64_t timeoutInMSecs = 0);
-	void releaseWriterLock();
+    void acquireWriterLock(bool wait = false, int64_t timeoutInMSecs = 0);
+    void releaseWriterLock();
 
-	IndexInfo acquireInfo();
-	void releaseInfo(const IndexInfo& info);
-	void updateInfo(const IndexInfo& oldInfo, const IndexInfo& newInfo, bool updateIndex = false);
+    IndexInfo acquireInfo();
+    void releaseInfo(const IndexInfo& info);
+    void updateInfo(const IndexInfo& oldInfo, const IndexInfo& newInfo, bool updateIndex = false);
 
-private:
-	ACOUSTID_DISABLE_COPY(Index);
+ private:
+    ACOUSTID_DISABLE_COPY(Index);
 
-	void acquireWriterLockInt(bool wait, int64_t timeoutInMSecs);
+    void acquireWriterLockInt(bool wait, int64_t timeoutInMSecs);
 
-	void open(bool create);
+    void open(bool create);
 
-	QMutex m_mutex;
-	DirectorySharedPtr m_dir;
-	bool m_hasWriter;
+    QMutex m_mutex;
+    DirectorySharedPtr m_dir;
+    bool m_hasWriter;
     QWaitCondition m_writerReleased;
-	std::unique_ptr<IndexFileDeleter> m_deleter;
-	IndexInfo m_info;
-	bool m_open;
+    std::unique_ptr<IndexFileDeleter> m_deleter;
+    IndexInfo m_info;
+    bool m_open;
 };
 
 typedef QWeakPointer<Index> IndexWeakPtr;
 typedef QSharedPointer<Index> IndexSharedPtr;
 
-}
+}  // namespace Acoustid
 
 #endif
