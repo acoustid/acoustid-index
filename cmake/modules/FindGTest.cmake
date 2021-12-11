012d28528c3771b5c42357f50df390efbdb3bc3e
Lukáš Lalinský <lalinsky@gmail.com>
2021-12-11 05:55:07 +0100

    Clean up gtest build

--- cmake/modules/FindGTest.cmake
+++ cmake/modules/FindGTest.cmake
@@ -26,37 +26,6 @@
 #
 #    enable_testing(true)
 #    add_test(AllTestsInFoo foo)
-#
-# =========================================================
-#
-# Copyright (C) 2012 Lukas Lalinsky <lalinsky@gmail.com>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#  * Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-#
-#  * Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#
-#  * The names of Kitware, Inc., the Insight Consortium, or the names of
-#    any consortium members, or of any contributors, may not be used to
-#    endorse or promote products derived from this software without
-#    specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 find_path(GTEST_SOURCE_DIR
@@ -79,4 +48,6 @@ if(GTEST_FOUND)
 	set(GTEST_LIBRARIES gtest)
 	set(GTEST_MAIN_LIBRARIES gtest_main)
     set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES})
+    set(GTEST_SOURCES ${GTEST_SOURCE_DIR}/src/gtest-all.cc)
+    set(GTEST_MAIN_SOURCES ${GTEST_SOURCE_DIR}/src/gtest_main.cc)
 endif()
