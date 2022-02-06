262784420d5844d13907df045cc6e1a84d1b9a1b
Lukáš Lalinský <lalinsky@gmail.com>
2022-02-06 17:46:29 +0100

    Implement gRPC search

--- src/server/grpc/service.cpp
+++ src/server/grpc/service.cpp
@@ -3,9 +3,34 @@
 namespace Acoustid {
 namespace Server {
 
-IndexServiceImpl::IndexServiceImpl(QSharedPointer<MultiIndex> indexes, QSharedPointer<Metrics> metrics) : m_indexes(indexes), m_metrics(metrics)
-{
+IndexServiceImpl::IndexServiceImpl(QSharedPointer<MultiIndex> indexes, QSharedPointer<Metrics> metrics)
+    : m_indexes(indexes), m_metrics(metrics) {}
+
+static inline int remainingTime(std::chrono::system_clock::time_point deadline) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(deadline - std::chrono::system_clock::now()).count();
 }
 
-} // namespace Server
-} // namespace Acoustid
+grpc::Status IndexServiceImpl::Search(grpc::ServerContext* context, const PB::SearchRequest* request,
+                                      PB::SearchResponse* response) {
+    auto indexName = QString::fromStdString(request->index_name());
+    std::vector<uint32_t> terms;
+    terms.assign(request->terms().begin(), request->terms().end());
+    try {
+        auto index = m_indexes->getIndex(indexName);
+        auto results = index->search(terms, remainingTime(context->deadline()));
+        for (auto result : results) {
+            if (response->results_size() >= request->max_results()) {
+                break;
+            }
+            auto r = response->add_results();
+            r->set_doc_id(result.docId());
+            r->set_score(result.score());
+        }
+    } catch (const IndexNotFoundException& e) {
+        return grpc::Status(grpc::NOT_FOUND, e.what());
+    }
+    return grpc::Status::OK;
+}
+
+}  // namespace Server
+}  // namespace Acoustid
--- src/server/grpc/service.h
+++ src/server/grpc/service.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <QSharedPointer>
+
 #include "index/multi_index.h"
-#include "server/metrics.h"
 #include "server/grpc/proto/index.grpc.pb.h"
+#include "server/metrics.h"
 
 namespace Acoustid {
 namespace Server {
@@ -12,10 +13,13 @@ class IndexServiceImpl final : public PB::Index::Service {
  public:
     IndexServiceImpl(QSharedPointer<MultiIndex> indexes, QSharedPointer<Metrics> metrics);
 
+    virtual ::grpc::Status Search(::grpc::ServerContext* context, const ::Acoustid::Server::PB::SearchRequest* request,
+                                  ::Acoustid::Server::PB::SearchResponse* response) override;
+
  private:
     QSharedPointer<MultiIndex> m_indexes;
     QSharedPointer<Metrics> m_metrics;
 };
 
-} // namespace Server
-} // namespace Acoustid
+}  // namespace Server
+}  // namespace Acoustid
