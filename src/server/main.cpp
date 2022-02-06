c559ba012a8f20f9761ad1146ac2541851e119a7
Lukáš Lalinský <lalinsky@gmail.com>
2022-02-06 14:06:47 +0100

    Reformat main.cpp

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,10 @@
 // Copyright (C) 2011  Lukas Lalinsky
 // Distributed under the MIT license, see the LICENSE file for details.
 
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+
 #include <QCoreApplication>
 #include <QThreadPool>
 
@@ -12,6 +16,7 @@
 #include "qhttpserver.hpp"
 #include "qhttpserverrequest.hpp"
 #include "qhttpserverresponse.hpp"
+#include "server/grpc/service.h"
 #include "store/fs_directory.h"
 #include "util/options.h"
 
@@ -20,76 +25,86 @@ using namespace Acoustid::Server;
 
 using namespace qhttp::server;
 
-int main(int argc, char **argv)
-{
-	OptionParser parser("%prog [options]");
-	parser.addOption("directory", 'd')
-		.setArgument()
-		.setHelp("index directory")
-		.setMetaVar("DIR")
-		.setDefaultValue(".");
-	parser.addOption("address", 'a')
-		.setArgument()
-		.setHelp("listen on this address (default: 127.0.0.1)")
-		.setDefaultValue("127.0.0.1");
-	parser.addOption("port", 'p')
-		.setArgument()
-		.setHelp("listen on this port (default: 6080)")
-		.setDefaultValue("6080");
-	parser.addOption("http-address")
-		.setArgument()
-		.setHelp("http server listens on this address (default: 127.0.0.1)")
+int main(int argc, char **argv) {
+    OptionParser parser("%prog [options]");
+
+    // clang-format off
+
+    parser.addOption("directory", 'd')
+        .setArgument()
+        .setHelp("index directory")
+        .setMetaVar("DIR")
+        .setDefaultValue(".");
+
+    parser.addOption("address", 'a')
+        .setArgument()
+        .setHelp("listen on this address (default: 127.0.0.1)")
+        .setDefaultValue("127.0.0.1");
+
+    parser.addOption("port", 'p')
+        .setArgument()
+        .setHelp("listen on this port (default: 6080)")
+        .setDefaultValue("6080");
+
+    parser.addOption("http-address")
+        .setArgument()
+        .setHelp("http server listens on this address (default: 127.0.0.1)")
         .setMetaVar("ADDRESS")
-		.setDefaultValue("127.0.0.1");
-	parser.addOption("http-port")
-		.setArgument()
-		.setHelp("http server listens on this port (default: 6081)")
+        .setDefaultValue("127.0.0.1");
+
+    parser.addOption("http-port")
+        .setArgument()
+        .setHelp("http server listens on this port (default: 6081)")
         .setMetaVar("PORT")
-		.setDefaultValue("6081");
-	parser.addOption("mmap", 'm')
-		.setHelp("use mmap to read index files");
-	parser.addOption("threads", 't')
-		.setArgument()
-		.setHelp("use specific number of threads")
-		.setDefaultValue("0");
-	std::unique_ptr<Options> opts(parser.parse(argc, argv));
+        .setDefaultValue("6081");
 
-	QString path = opts->option("directory");
+    parser.addOption("mmap", 'm')
+        .setHelp("use mmap to read index files");
 
-	QString address = opts->option("address");
-	int port = opts->option("port").toInt();
+    parser.addOption("threads", 't')
+        .setArgument()
+        .setHelp("use specific number of threads")
+        .setDefaultValue("0");
 
-	QString httpAddress = opts->option("http-address");
-	int httpPort = opts->option("http-port").toInt();
+    // clang-format on
 
-	QCoreApplication app(argc, argv);
+    std::unique_ptr<Options> opts(parser.parse(argc, argv));
 
-	int numThreads = opts->option("threads").toInt();
-	if (numThreads) {
-		QThreadPool::globalInstance()->setMaxThreadCount(numThreads);
-	}
+    QString path = opts->option("directory");
 
-	auto indexes = QSharedPointer<MultiIndex>::create();
-	auto metrics = QSharedPointer<Metrics>::create();
+    QString address = opts->option("address");
+    int port = opts->option("port").toInt();
 
-	Listener::setupSignalHandlers();
+    QString httpAddress = opts->option("http-address");
+    int httpPort = opts->option("http-port").toInt();
 
-	Listener listener(path, opts->contains("mmap"));
-	listener.setMetrics(metrics);
-	listener.listen(QHostAddress(address), port);
-	qDebug() << "Simple server listening on" << address << "port" << port;
+    QCoreApplication app(argc, argv);
+
+    int numThreads = opts->option("threads").toInt();
+    if (numThreads) {
+        QThreadPool::globalInstance()->setMaxThreadCount(numThreads);
+    }
+
+    auto indexes = QSharedPointer<MultiIndex>::create();
+    auto metrics = QSharedPointer<Metrics>::create();
+
+    Listener::setupSignalHandlers();
+
+    Listener listener(path, opts->contains("mmap"));
+    listener.setMetrics(metrics);
+    listener.listen(QHostAddress(address), port);
+    qDebug() << "Simple server listening on" << address << "port" << port;
 
     indexes->addIndex("main", listener.index());
 
     HttpRequestHandler handler(listener.index(), metrics);
 
-	QHttpServer httpListener(&app);
+    QHttpServer httpListener(&app);
     httpListener.listen(QHostAddress(httpAddress), httpPort, [&](QHttpRequest *req, QHttpResponse *res) {
         handler.router().handle(req, res);
     });
     qDebug() << "HTTP server listening on" << httpAddress << "port" << httpPort;
     qDebug() << "Prometheus metrics available at" << QString("http://%1:%2/_metrics").arg(httpAddress).arg(httpPort);
 
-	return app.exec();
+    return app.exec();
 }
-
