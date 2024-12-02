2be9078eea91b8d5b888d425662c77f20724b0b8
Lukas Lalinsky <llalinsky@live365.com>
2024-12-02 22:48:13 +0100

    Add msgpack HTTP API tests

--- tests/test_index_api.py
+++ tests/test_index_api.py
@@ -1,4 +1,6 @@
+import pytest
 import json
+import msgpack
 
 
 def test_head_index_not_found(client, index_name):
@@ -19,39 +21,71 @@ def test_get_index_not_found(client, index_name):
     assert json.loads(req.content) == {'error': 'IndexNotFound'}
 
 
-def test_get_index(client, index_name, create_index):
-    req = client.get(f'/{index_name}')
+@pytest.mark.parametrize('fmt', ['json', 'msgpack'])
+def test_get_index(client, index_name, create_index, fmt):
+    req = client.get(f'/{index_name}', headers=headers(fmt))
     assert req.status_code == 200, req.content
-    assert json.loads(req.content) == {'version': 0, 'attributes': []}
+
+    if fmt == 'json':
+        expected = {'version': 0, 'attributes': []}
+    else:
+        expected = {'v': 0, 'a': {}}
+    assert decode(fmt, req.content) == expected
 
 
-def test_create_index(client, index_name):
+def headers(fmt):
+    if fmt == 'json':
+        return {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    elif fmt == 'msgpack':
+        return {
+            'Content-Type': 'application/vnd.msgpack',
+            'Accept': 'application/vnd.msgpack',
+        }
+    else:
+        return {}
+
+
+def decode(fmt, content):
+    if fmt == 'json':
+        return json.loads(content)
+    elif fmt == 'msgpack':
+        return msgpack.loads(content)
+    else:
+        assert False
+
+
+@pytest.mark.parametrize('fmt', ['json', 'msgpack'])
+def test_create_index(client, index_name, fmt):
     req = client.head(f'/{index_name}')
     assert req.status_code == 404, req.content
 
-    req = client.put(f'/{index_name}')
+    req = client.put(f'/{index_name}', headers=headers(fmt))
     assert req.status_code == 200, req.content
-    assert json.loads(req.content) == {}
+    assert decode(fmt, req.content) == {}
 
-    req = client.put(f'/{index_name}')
+    req = client.put(f'/{index_name}', headers=headers(fmt))
     assert req.status_code == 200, req.content
-    assert json.loads(req.content) == {}
+    assert decode(fmt, req.content) == {}
 
     req = client.head(f'/{index_name}')
     assert req.status_code == 200, req.content
 
 
-def test_delete_index(client, index_name, create_index):
+@pytest.mark.parametrize('fmt', ['json', 'msgpack'])
+def test_delete_index(client, index_name, create_index, fmt):
     req = client.head(f'/{index_name}')
     assert req.status_code == 200, req.content
 
-    req = client.delete(f'/{index_name}')
+    req = client.delete(f'/{index_name}', headers=headers(fmt))
     assert req.status_code == 200, req.content
-    assert json.loads(req.content) == {}
+    assert decode(fmt, req.content) == {}
 
-    req = client.delete(f'/{index_name}')
+    req = client.delete(f'/{index_name}', headers=headers(fmt))
     assert req.status_code == 200, req.content
-    assert json.loads(req.content) == {}
+    assert decode(fmt, req.content) == {}
 
     req = client.head(f'/{index_name}')
     assert req.status_code == 404, req.content
