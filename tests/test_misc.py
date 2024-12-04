099b392fb085a1d86c4cabe72eb1a11eeed2b583
Lukas Lalinsky <llalinsky@live365.com>
2024-12-04 11:45:18 +0100

    Add index ping test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -4,6 +4,12 @@ def test_ping(client):
     assert 'pong' in req.text
 
 
+def test_index_ping(client, index_name, create_index):
+    req = client.get(f'{index_name}/_ping')
+    assert req.status_code == 200, req.content
+    assert 'pong' in req.text
+
+
 def test_metrics(client):
     req = client.get(f'/_metrics')
     assert req.status_code == 200, req.content
